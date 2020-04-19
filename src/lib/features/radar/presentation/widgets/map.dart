b1e38115934c6e327e540229a1b5f2359b0c5d36
m.konecnik <marusa.konecnik@ineor.si>
2020-04-19 17:17:34 +0200

    current location added

--- src/lib/features/radar/presentation/widgets/map.dart
+++ src/lib/features/radar/presentation/widgets/map.dart
@@ -2,7 +2,10 @@ import 'dart:async';
 
 import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:location/location.dart';
 import 'package:shoodar/features/radar/domain/entitites/radar.dart';
+
+const double CAMERA_ZOOM = 16;
 
 class Map extends StatefulWidget {
   static const LatLng _center = const LatLng(46.056946, 14.505751);
@@ -20,26 +23,59 @@ class Map extends StatefulWidget {
 
 class _MapState extends State<Map> {
   final Completer<GoogleMapController> _controller = Completer();
+  LocationData currentLocation;
+  Location location;
 
+  BitmapDescriptor pinLocationIcon;
   final Set<Marker> _markers = {};
 
   @override
+  void initState() {
+      super.initState();
+
+      setCustomMapPin();
+
+      location = new Location();
+ 
+      location.onLocationChanged.listen((LocationData cLoc) {
+        currentLocation = cLoc;
+      });
+
+      setInitialLocation();
+     
+  }
+
+  void setInitialLocation() async {
+    currentLocation = await location.getLocation();
+  }
+
+  void setCustomMapPin() async {
+  pinLocationIcon = await BitmapDescriptor.fromAssetImage(
+      ImageConfiguration(devicePixelRatio: 2.5),
+      'assets/icons8-police-car-64.png');
+}
+
+  @override
   Widget build(BuildContext context) {
+    CameraPosition initialCameraPosition = CameraPosition(
+        zoom: 7.0,
+        target: LatLng(46.056946,  14.505751));
+
     return Scaffold(
         body: GoogleMap(
+          myLocationButtonEnabled: true,
+          myLocationEnabled: true,
+          compassEnabled: true,
           onMapCreated: _onMapCreated,
           markers: _markers,
-          initialCameraPosition: CameraPosition(
-            target: Map._center,
-            zoom: 7.0)
+          initialCameraPosition: initialCameraPosition
             )
       );
   }
 
    void _onMapCreated(GoogleMapController controller) {
       _addMarkers();
-      _controller.complete(controller);
-    
+      _controller.complete(controller); 
   }
 
   void _addMarkers() {
@@ -52,7 +88,7 @@ class _MapState extends State<Map> {
           title: 'Created at:',
           snippet: radar.timeCreated.toString(),
         ),
-        icon: BitmapDescriptor.defaultMarker,
+        icon: pinLocationIcon
       ))    
     });
     });
